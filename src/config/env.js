5e578d0cac2a82662cd2557ff0d8c08d80ba44c2
Anuj upadhyay <153272046+AnujUpadhyay18@users.noreply.github.com>
2025-04-25 22:30:00 +0530

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_URL || "mongodb+srv://eparkgateway:qsb3O2PbQzMUysvX@epark.vrqna2i.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=Epark",
+  DB_URL: process.env.DB_URL || "mongodb+srv://eparkgateway:yhWTBnjuXUwoQ5Fr@cluster0.qk4b2sv.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=Cluster0",
 
 };
