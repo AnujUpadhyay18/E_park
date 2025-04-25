4889911c64b8c9fe0eed3bc9ac85c3a1debc7c87
Anuj Upadhyay <anujbca2022@gmail.com>
2025-04-25 12:31:49 +0530

    updated

--- src/config/env.js
+++ src/config/env.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_URL || "mongodb://localhost:27017/NEWDATA",
+  DB_URL: process.env.DB_URL || "mongodb+srv://eparkgateway:qsb3O2PbQzMUysvX@epark.vrqna2i.mongodb.net/NEWDATA?retryWrites=true&w=majority&appName=Epark",
+
 };
