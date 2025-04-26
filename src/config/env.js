45f68cc1fbc7cc909bae20bafa4242386b65f471
Anuj upadhyay <153272046+AnujUpadhyay18@users.noreply.github.com>
2025-04-26 07:39:27 +0530

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_URL ||  mongodb+srv://bholebaba:5TlRZHeUaPYEW2AD@cluster0.fmkrt.mongodb.net/data?retryWrites=true&w=majority&appName=Cluster0"
+  DB_URL: process.env.DB_URL ||  "mongodb+srv://bholebaba:5TlRZHeUaPYEW2AD@cluster0.fmkrt.mongodb.net/data?retryWrites=true&w=majority&appName=Cluster0"
 
 };
